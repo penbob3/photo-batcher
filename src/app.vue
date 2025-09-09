7ff8b66be9c364b16e64930a85b75caff0fd31b9
penbob3 <plazmacatchermc@gmail.com>
2025-09-09 13:25:29 +1000

    Added Top Menu

---
+++ src/app.vue
@@ -0,0 +1,22 @@
+<template>
+    <PageSwitcher :menuOptions="menuOptions"></PageSwitcher>
+    <h1>💖 Hello World!</h1>
+    <p>Welcome to your Electron application.</p>
+</template>
+  
+<script>
+    import PageSwitcher from './components/PageSwitcher.vue'
+
+    export default {
+        components: {
+            PageSwitcher
+        },
+        data() {
+            return {
+                menuOptions: ["Browser", "Review"]
+            }
+        }
+    }
+
+    console.log('👋 This message is being logged by "App.vue", included via Vite');
+</script>
---
+++ src/components/PageSwitcher.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="container" class="roboto-300">
+        <div class="spacer"></div>
+        <div v-for="options,idx in menuOptions" @click="selectMenuOption(idx)" :class="'option ' + (selectedMenuOption == idx ? 'selected' : '')">{{ options }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            menuOptions: Array
+        },
+        data() {
+            return {
+                selectedMenuOption: 0
+            }
+        },
+        methods: {
+            selectMenuOption: function(optionIdx) {
+                this.selectedMenuOption = optionIdx
+                this.$emit('menuChange', optionIdx)
+            }
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+    #container {
+        height: 50px;
+        border-bottom: 1px black solid;
+        background-color: rgb(71, 212, 165);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        user-select: none;
+    }
+    
+    .option {
+        height: 49px;
+        width: 110px;
+        text-align: center;
+        line-height: 48px;
+        border-right: 1px solid black;
+    }
+
+    .spacer {
+        height: 49px;
+        background-color: black;
+        width: 1px;
+    }
+
+    .selected {
+        background-color: rgb(116, 255, 209);
+    }
+</style>
